ac25ddf43b5ad30ea98f985d0875331660f0d46b
Takuya Uehara <bb38122012@ms.nagasaki-u.ac.jp>
2024-09-09 19:43:15 +0900

    shorter time string

--- backend/models.py
+++ backend/models.py
@@ -10,5 +10,6 @@ class Diary(Base):
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String)
     body = Column(String)
-    created_at = Column(String, default=datetime.now(pytz.timezone('Asia/Tokyo')))
-    updated_at = Column(String, default=datetime.now(pytz.timezone('Asia/Tokyo')), onupdate=datetime.now(pytz.timezone('Asia/Tokyo')))
+    created_at = Column(String, default=datetime.now(pytz.timezone('Asia/Tokyo')).strftime('%Y-%m-%d %H:%M'))
+    updated_at = Column(String, default=datetime.now(pytz.timezone('Asia/Tokyo')).strftime('%Y-%m-%d %H:%M'),
+                         onupdate=datetime.now(pytz.timezone('Asia/Tokyo')).strftime('%Y-%m-%d %H:%M'))
