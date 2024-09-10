f0df50e18a9079425950d665bfab3b68bb6e4246
Takuya Uehara <bb38122012@ms.nagasaki-u.ac.jp>
2024-09-10 16:35:33 +0900

    changed to class based crud

--- backend/crud.py
+++ backend/crud.py
@@ -2,33 +2,39 @@ from sqlalchemy.orm import Session
 from . import models, schemas
 
 
-def get_diary(db: Session, diary_id: int):
-    return db.query(models.Diary).filter(models.Diary.id == diary_id).first()
+class CRUDBase:
+    def __init__(self, db: Session):
+        self.db = db
 
 
-def get_diaries(db: Session, skip: int = 0, limit: int = 100):
-    return db.query(models.Diary).offset(skip).limit(limit).all()
-
-
-def create_diary(db: Session, diary: schemas.DiaryCreate):
-    db_diary = models.Diary(title=diary.title, body=diary.body)
-    db.add(db_diary)
-    db.commit()
-    db.refresh(db_diary)
-    return db_diary
-
-
-def update_diary(db: Session, diary_id: int, diary: schemas.DiaryCreate):
-    db_diary = db.query(models.Diary).filter(models.Diary.id == diary_id).first()
-    db_diary.title = diary.title
-    db_diary.body = diary.body
-    db.commit()
-    db.refresh(db_diary)
-    return db_diary
-
-
-def delete_diary(db: Session, diary_id: int):
-    db_diary = db.query(models.Diary).filter(models.Diary.id == diary_id).first()
-    db.delete(db_diary)
-    db.commit()
-    return db_diary
+class DiaryCRUD(CRUDBase):
+    def __init__(self, db: Session, model: models.Diary):
+        super().__init__(db)
+        self.model = model
+    
+    def get_diary(self, diary_id: int):
+        return self.db.query(self.model).filter(self.model.id == diary_id).first()
+    
+    def get_diaries(self, skip: int = 0, limit: int = 100):
+        return self.db.query(self.model).offset(skip).limit(limit).all()
+    
+    def create_diary(self, diary: schemas.DiaryCreate):
+        db_diary = self.model(title=diary.title, body=diary.body)
+        self.db.add(db_diary)
+        self.db.commit()
+        self.db.refresh(db_diary)
+        return db_diary
+    
+    def update_diary(self, diary_id: int, diary: schemas.DiaryCreate):
+        db_diary = self.db.query(self.model).filter(self.model.id == diary_id).first()
+        db_diary.title = diary.title
+        db_diary.body = diary.body
+        self.db.commit()
+        self.db.refresh(db_diary)
+        return db_diary
+    
+    def delete_diary(self, diary_id: int):
+        db_diary = self.db.query(self.model).filter(self.model.id == diary_id).first()
+        self.db.delete(db_diary)
+        self.db.commit()
+        return db_diary
--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,16 @@
-from . import crud, schemas, models
+from . import schemas, models
+from .crud import DiaryCRUD
 from .database import SessionLocal, engine
 from typing import List
 from sqlalchemy.orm import Session
 from fastapi import Depends, FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
+from dotenv import load_dotenv
+
+load_dotenv()
 
 models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
-
 
 # Dependency
 def get_db():
@@ -38,28 +41,39 @@ async def Hello():
 
 @app.get("/diaries/", response_model=List[schemas.Diary])
 async def read_diaries(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    diaries = crud.get_diaries(db, skip=skip, limit=limit)
+    diary_crud = DiaryCRUD(db=db, model=models.Diary)
+    diaries = diary_crud.get_diaries(skip=skip, limit=limit)
+
     return diaries
 
 
 @app.get("/diaries/{diary_id}", response_model=schemas.Diary)
 async def read_diary(diary_id: int, db: Session = Depends(get_db)):
-    db_diary = crud.get_diary(db, diary_id=diary_id)
+    diary_crud = DiaryCRUD(db=db, model=models.Diary)
+    db_diary = diary_crud.get_diary(diary_id=diary_id)
+
     if db_diary is None:
         raise HTTPException(status_code=404, detail="Diary not found")
+
     return db_diary
 
 
 @app.post("/diaries/", response_model=schemas.Diary)
 async def create_diary(diary: schemas.DiaryCreate, db: Session = Depends(get_db)):
-    return crud.create_diary(db=db, diary=diary)
+    diary_crud = DiaryCRUD(db=db, model=models.Diary)
+
+    return diary_crud.create_diary(diary=diary)
 
 
 @app.put("/diaries/{diary_id}", response_model=schemas.Diary)
 async def update_diary(diary_id: int, diary: schemas.DiaryCreate, db: Session = Depends(get_db)):
-    return crud.update_diary(db=db, diary_id=diary_id, diary=diary)
+    diary_crud = DiaryCRUD(db=db, model=models.Diary)
+
+    return diary_crud.update_diary(diary_id=diary_id, diary=diary)
 
 
 @app.delete("/diaries/{diary_id}", response_model=schemas.Diary)
 async def delete_diary(diary_id: int, db: Session = Depends(get_db)):
-    return crud.delete_diary(db=db, diary_id=diary_id)
+    diary_crud = DiaryCRUD(db=db, model=models.Diary)
+
+    return diary_crud.delete_diary(diary_id=diary_id)
